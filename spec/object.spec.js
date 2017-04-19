316380b0021f31558a8705987f3066d36f4ae56f
Slava Pavlutin <sl.pavlutin@gmail.com>
2017-04-19 22:25:09 +0300

    Test object literals

--- spec/object.spec.js
+++ spec/object.spec.js
@@ -1,6 +1,66 @@
 import { expect } from 'chai';
 
 describe('Object', () => {
+  describe('literals', () => {
+    it('allow to use property value shorthands', () => {
+      const x = 'x';
+      const y = 'y';
+      const obj = { x, y };
+      expect(obj).to.eql({ x: 'x', y: 'y' });
+    });
+
+    it('allow to use computed properties', () => {
+      const name = 'test';
+      const obj = {
+        [name]: 'test',
+      };
+      expect(obj.test).to.eq('test');
+    });
+
+    it('allow to define methods', () => {
+      const obj = {
+        self() {
+          return this;
+        },
+      };
+      expect(obj.self()).to.eq(obj);
+    });
+
+    it('allow to define getters', () => {
+      const obj = {
+        hiddenName: 'test',
+        get name() {
+          return this.hiddenName;
+        },
+      };
+      expect(obj.name).to.eq('test');
+    });
+
+    it('allow to define setters', () => {
+      const obj = {
+        hiddenName: 'test',
+        get name() {
+          return this.hiddenName;
+        },
+        set name(value) {
+          this.hiddenName = value;
+        },
+      };
+      obj.name = 'new';
+      expect(obj.name).to.eq('new');
+    });
+
+    it('allow to define generators', () => {
+      const obj = {
+        data: 'test',
+        * gen() {
+          yield this.data;
+        },
+      };
+      expect(obj.gen().next()).to.eql({ value: 'test', done: false });
+    });
+  });
+
   describe('.getOwnPropertyNames()', () => {
     it('gets own property names', () => {
       const obj = { test: 'test' };
